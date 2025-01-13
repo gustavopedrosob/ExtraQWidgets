b70ef8b4b193551d0721674f56a44d902e56b0d8
Gustavo <thevicio27@gmail.com>
2025-01-13 11:39:56 -0300

    update: added categories

--- qemojipicker.py
+++ qemojipicker.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QWidget, QLineEdit, QHBoxLayout, QLabel, QGridLayout
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QWidget, QLineEdit, QHBoxLayout, QLabel, QGridLayout, QVBoxLayout, QPushButton
 
 
 class QEmojiPicker(QWidget):
@@ -12,10 +13,21 @@ class QEmojiPicker(QWidget):
 
         self.__current_emoji_label = QLabel()
 
+        self.__menu_horizontal_layout = QHBoxLayout()
+
+        for icon in ["face-smile", "leaf", "bowl-food", "gamepad", "bicycle", "lightbulb", "heart", "flag"]:
+            button = QPushButton()
+            button.setIcon(QIcon(f"assets/icons/{icon}-solid.svg"))
+            self.__menu_horizontal_layout.addWidget(button)
+
         self.__horizontal_layout = QHBoxLayout()
         self.__horizontal_layout.addWidget(self.__line_edit)
         self.__horizontal_layout.addLayout(self.__grid_layout)
         self.__horizontal_layout.addWidget(self.__current_emoji_label)
 
-        self.setLayout(self.__horizontal_layout)
+        self.__vertical_layout = QVBoxLayout()
+        self.__vertical_layout.addLayout(self.__menu_horizontal_layout)
+        self.__vertical_layout.addLayout(self.__horizontal_layout)
+
+        self.setLayout(self.__vertical_layout)
 
