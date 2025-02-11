9598bd2f04b39803c87414f414d032ff6742bff9
Gustavo <thevicio27@gmail.com>
2025-02-11 15:51:19 -0300

    fix: color button test title

--- tests/color_button_test.py
+++ tests/color_button_test.py
@@ -9,7 +9,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Collapse Group Test")
+        self.setWindowTitle("Color Button Test")
 
         widget = QWidget()
 
