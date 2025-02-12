84e2a713743ac8d2056828243abe58766339c545
Gustavo <thevicio27@gmail.com>
2025-02-11 21:22:45 -0300

    fix: removing long description

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="extra_qwidgets",
     version="0.0.1",
     author="gustavopedrosob",
     author_email="thevicio27@gmail.com",
     description="Extra widgets for Qt/PySide6",
-    long_description=long_description,
     url="https://github.com/gustavopedrosob/ExtraQWidgets",
     project_urls={
         "Bug Tracker": "https://github.com/gustavopedrosob/ExtraQWidgets/issues",
