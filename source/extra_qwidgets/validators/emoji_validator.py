099412d76472149361d6659aff657a7cdbb9ba71
gustavopedrosob <thevicio27@gmail.com>
2025-05-07 10:57:22 -0300

    fix: emoji validator

--- source/extra_qwidgets/validators/emoji_validator.py
+++ source/extra_qwidgets/validators/emoji_validator.py
@@ -1,10 +1,11 @@
 from PySide6.QtGui import QValidator
+from emoji import is_emoji, purely_emoji
 from emojis import emojis
 
 
 class QEmojiValidator(QValidator):
     def validate(self, input_str: str, pos: int):
-        if emojis.count(input_str) == len(input_str):
+        if purely_emoji(input_str):
             return QValidator.State.Acceptable, input_str, pos
         else:
             return QValidator.State.Invalid, input_str, pos
